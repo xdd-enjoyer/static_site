7b2f17582d0b2d83bb522bb3437c060b39c0a8d0
xdd-enjoyer <daniel.barnett42@gmail.com>
2026-07-04 15:01:34 +0100

    added web page generation

---
+++ src/gencontent.py
@@ -0,0 +1,43 @@
+import os
+
+from markdown_blocks import markdown_to_html_node
+
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
+
+def extract_title(md: str) -> str:
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
+
+def generate_pages_recursively(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+    for entry in os.listdir(dir_path_content):
+        path = os.path.join(dir_path_content, entry)
+        if os.path.isfile(path) == True:
+            generate_page(os.path.join(path), template_path, os.path.join(dest_dir_path, "index.html"))
+        else:
+            generate_pages_recursively(os.path.join(dir_path_content, entry), template_path, os.path.join(dest_dir_path, entry))
--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
+import os
+import shutil
 
-# hello world
-from textnode import * 
+from copystatic import copy_files_recursive
+from gencontent import *
 
-def main():
-    textnode = TextNode(1, 2, 3)
-    print(textnode.__repr__())
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
+
+def main() -> None:
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating pages...")
+    generate_pages_recursively(
+        dir_path_content,
+        template_path,
+        dir_path_public
+        )
+
 
 main()
